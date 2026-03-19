57d3d807cea8dfaf8cf3d1f61f8b5d94e83e6d07
Krishil-coder <krishilshah70@gmail.com>
2026-03-19 11:38:26 +0530

    Added JavaScript Assignment

---
+++ JavaScript/Assignment.js
@@ -0,0 +1,129 @@
+
+//Assignment 1: Smart Discount Calculator
+
+// let userName ='Suresh'
+// let age =7
+// let purchaseAmount = 5000
+// let finalAmount=0;
+// let DiscountAmount=0;
+// let DiscountPercent=0;
+
+// if(age<18  ){
+//      DiscountPercent +=10
+// //      finalAmount=purchaseAmount-DiscountAmount
+// // console.log(userName + "10% discount" + finalAmount)   
+// }else if(age>=60){
+//      DiscountPercent +=20
+// }
+// if(purchaseAmount>=5000){
+//      DiscountPercent +=5
+//      // finalAmount=purchaseAmount-DiscountAmount
+//      // console.log(userName + "5% discount" + finalAmount)
+// }
+// DiscountAmount = (purchaseAmount*DiscountPercent)/100
+// finalAmount = purchaseAmount -DiscountAmount;
+
+// console.log(`hi ${userName}! you received total discount of ${DiscountPercent}%. FinalAmount :${finalAmount}`)
+
+// //console.log(userName + DiscountAmount + finalAmount);
+
+
+// ;
+
+//Assignment 2: Login Validation System
+
+// let  username ="Krishil"
+// let  password = "Rishabh@2026"
+
+// let  userInputNAme="Krishil"
+// let  userInputPassword="Rishabh@2026"
+
+// if(username===userInputNAme && password===userInputPassword){
+//      console.log("Login Successful!")
+// }
+// else if(username!==userInputNAme || password===userInputPassword ){
+//      console.log("username is invalid")
+// }
+// else if(username!==userInputNAme ||  password!==userInputPassword){
+//      console.log("password is invalid")
+// }
+// else{
+//      console.log("Both userName and password  are Incorrect")
+// }
+
+//Assignment 3: Number Analyzer Tool
+
+// const Number = -4
+
+// if(Number==0){
+//      console.log("Zero")
+// }
+// else if(Number>0){
+//      console.log("Positive Number")
+// }
+// else{
+//       console.log("Negative Number")
+// }
+
+// if(Number%2==0){
+//      console.log("Even Number")
+// }else{
+//       console.log("Odd Number")
+// }
+
+//Assignment 4: Shopping Eligibility Checker
+
+     // const isMember=true
+     // const cartValue =3000
+
+     // if(isMember == true && cartValue>1000 || cartValue>2000 ){
+     //      console.log("You are Eligible for free delivery")
+     // }
+     // else{
+     //       console.log("You are not Eligible for free delivery")
+     // }
+
+     // Assignment 5: Grade Evaluation System
+
+     let marks =78
+
+     if(marks>90){
+           console.log("You grade is A")
+     }
+     else if(marks>75 && marks<89){
+          console.log("You grade is B")
+     }
+     else if(marks>50 && marks<74){
+          console.log("You grade is C")
+     }
+     else if(marks<50){
+           console.log("You are fail")
+     }
+     else{
+          console.log("Invalid Output")
+     }
+
+     // let SureshMass =55;
+// let SureshHeight = 1.7;
+
+// let RameshMass =60;
+// let RameshHeight = 1.65;
+
+// let BMI = SureshMass/SureshHeight **2;
+// let BMI1= RameshMass/RameshHeight **2;
+// console.log(BMI + " " + "Suresh BMI");
+// console.log(BMI1 + " " + "Ramesh BMI");
+
+// let markHigherBMI = false;
+
+// if(BMI<BMI1){
+//      markHigherBMI = true;
+//      console.log( "Ramesh has more bmi");
+// }
+// else{
+//     markHigherBMI = false;
+//      console.log("Suresh has more bmi");
+// }
+
+
+//  console.log(markHigherBMI + "");
